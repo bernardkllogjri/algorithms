a2fe0f7d16545b581eb6356872a777a7e7dae373
Bernard Kllogjri <bernardkllogjri@gmail.com>
2022-01-12 00:39:43 +0100

    Simple calculator | refactoring

--- simple-calculator.ts
+++ simple-calculator.ts
@@ -1,27 +1,20 @@
+type Operator = '+' | '*' | '';
+
 /**
  * A simple calculation evaluating addition and multiplication mathematical expression
  * @param {string} expr - Mathematical expression
  * @returns {number} - The result of the mathematical expression
  */
-
-type Operator = '+' | '*' | '';
-
 const calculator = (expr: string) => {
   const elements: string[] = expr.split(' ')
   let operator: Operator = '';
   let acc: number = -1;
   if(expr[0] !== '(') return parseInt(expr);
   for(let i = 0; i < elements.length; i++) {
-    if(['+', '*'].includes(elements[i])) operator = elements[i] as Operator
-    if(elements[i] === '+') {
-      operator = '+';
-      acc = 0;
-    } 
-    else if(elements[i] === '*') {
-      operator = '*';
-      acc = 1;
-    } 
-    else if(elements[i] !== '(' && elements[i] !== ')') {
+    if(['+', '*'].includes(elements[i])) {
+      operator = elements[i] as Operator
+      acc = operator === '+' ? 0 : 1
+    } else if(!['(', ')'].includes(elements[i])) {
       if(operator === '+') acc += parseInt(elements[i]);
       if(operator === '*') acc *= parseInt(elements[i]);
     } else if(elements[i] === '(' && i !== 0) {
